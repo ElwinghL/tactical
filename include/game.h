d1cf7e98cc7b4c4fbbcb053caa727f3a955fd52d
Fabien Matusalem <matusalemf@aol.com>
2018-10-29 19:10:33 +0100

    Ajout du menu principal et des états du jeu

--- include/game.h
+++ include/game.h
@@ -6,14 +6,23 @@
 #include <gf/Color.h>
 #include <gf/EntityContainer.h>
 #include <gf/Event.h>
+#include <gf/Font.h>
 #include <gf/RenderWindow.h>
+#include <gf/Text.h>
 #include <gf/ViewContainer.h>
 #include <gf/Views.h>
+#include <gf/WidgetContainer.h>
+#include <gf/Widgets.h>
 #include <gf/Window.h>
 
 class Game {
 public:
     Game();
+    Game(const Game& other) = delete;
+    Game(Game&& other) = default;
+
+    Game& operator=(const Game& other) = delete;
+    Game& operator=(Game&& other) = default;
 
     bool isRunning();
 
@@ -22,30 +31,56 @@ public:
     void render();
 
 private:
+    enum class GameState {
+        MainMenu,
+        Pause,
+        GameStart,
+        PlayerTurn,
+        WaitingForAI,
+        GameEnd
+    };
+
     void initWindow();
     void initViews();
     void initActions();
+    void initWidgets();
     void initEntities();
 
     const gf::Vector2u m_screenSize {1024, 576};
     const gf::Vector2f m_viewSize {100.0f, 100.0f};
     const gf::Vector2f m_viewCenter {0.0f, 0.0f};
 
-    const gf::Vector4f m_clearColor {gf::Color::Black};
+    gf::Vector4f m_clearColor {gf::Color::White};
+
+    gf::Vector2f m_mouseCoords {};
+
+    GameState m_gameState {GameState::MainMenu};
 
     gf::Window m_window {"Cthulhu vs Satan", m_screenSize};
     gf::RenderWindow m_renderer {m_window};
 
     gf::ViewContainer m_views {};
     gf::ExtendView m_mainView {m_viewCenter, m_viewSize};
-    gf::ScreenView m_hudView {};
+    gf::ExtendView m_menuView {m_viewCenter, gf::Vector2f{800.0f, 450.0f}};
 
     gf::ActionContainer m_actions {};
     gf::Action m_closeWindowAction {"Close window"};
     gf::Action m_fullscreenAction {"Fullscreen"};
+    gf::Action m_leftClickAction {"Left click"};
+
+    gf::Font m_buttonFont { [] {
+        gf::Font font {};
+        font.loadFromFile("../assets/fonts/button.ttf");
+        return font;
+    } ()};
+
+    gf::Text m_title {"Cthulhu vs Satan", m_buttonFont};
+
+    gf::WidgetContainer m_menuWidgets {};
+    gf::TextButtonWidget m_playButton {"Play!", m_buttonFont};
+    gf::TextButtonWidget m_quitButton {"Quit", m_buttonFont};
 
     gf::EntityContainer m_mainEntities {};
-    gf::EntityContainer m_hudEntities {};
 
     gf::Clock m_clock {};
 };
--- src/game.cpp
+++ src/game.cpp
@@ -1,13 +1,14 @@
 #include "game.h"
 
+#include <iostream>
+
 Game::Game()
 {
     initWindow();
     initViews();
     initActions();
+    initWidgets();
     initEntities();
-
-    m_renderer.clear(m_clearColor);
 }
 
 bool Game::isRunning()
@@ -20,6 +21,10 @@ void Game::processEvents()
     gf::Event event;
 
     while (m_window.pollEvent(event)) {
+        if (event.type == gf::EventType::MouseMoved) {
+            m_mouseCoords = m_renderer.mapPixelToCoords(event.mouseCursor.coords);
+        }
+
         m_actions.processEvent(event);
         m_views.processEvent(event);
     }
@@ -32,25 +37,124 @@ void Game::processEvents()
         m_window.toggleFullscreen();
     }
 
+    switch (m_gameState) {
+    case GameState::MainMenu: {
+        if (m_leftClickAction.isActive()) {
+            m_menuWidgets.pointTo(m_mouseCoords);
+            if (m_playButton.contains(m_mouseCoords) || m_quitButton.contains(m_mouseCoords)) {
+                m_menuWidgets.triggerAction();
+            }
+        }
+    }
+    break;
+
+    case GameState::Pause: {
+
+    }
+    break;
+
+    case GameState::GameStart: {
+
+    }
+    break;
+
+    case GameState::PlayerTurn: {
+
+    }
+    break;
+
+    case GameState::WaitingForAI: {
+
+    }
+    break;
+
+    case GameState::GameEnd: {
+
+    }
+    break;
+    }
+
     m_actions.reset();
 }
 
 void Game::update()
 {
     gf::Time time = m_clock.restart();
-    m_mainEntities.update(time);
-    m_hudEntities.update(time);
+    switch (m_gameState) {
+    case GameState::MainMenu: {
+
+    }
+    break;
+
+    case GameState::Pause: {
+
+    }
+    break;
+
+    case GameState::GameStart: {
+        m_clearColor = gf::Color::Black;
+
+        m_gameState = GameState::PlayerTurn;
+    }
+    break;
+
+    case GameState::PlayerTurn: {
+        m_mainEntities.update(time);
+    }
+    break;
+
+    case GameState::WaitingForAI: {
+        m_mainEntities.update(time);
+    }
+    break;
+
+    case GameState::GameEnd: {
+
+    }
+    break;
+    }
 }
 
 void Game::render()
 {
-    m_renderer.clear();
+    m_renderer.clear(m_clearColor);
 
-    m_renderer.setView(m_mainView);
-    m_mainEntities.render(m_renderer);
+    switch (m_gameState) {
+    case GameState::MainMenu: {
+        m_renderer.setView(m_menuView);
+        m_renderer.draw(m_title);
+        m_menuWidgets.render(m_renderer);
+    }
+    break;
 
-    m_renderer.setView(m_hudView);
-    m_hudEntities.render(m_renderer);
+    case GameState::Pause: {
+        m_renderer.setView(m_mainView);
+        m_mainEntities.render(m_renderer);
+    }
+    break;
+
+    case GameState::GameStart: {
+
+    }
+    break;
+
+    case GameState::PlayerTurn: {
+        m_renderer.setView(m_mainView);
+        m_mainEntities.render(m_renderer);
+    }
+    break;
+
+    case GameState::WaitingForAI: {
+        m_renderer.setView(m_mainView);
+        m_mainEntities.render(m_renderer);
+    }
+    break;
+
+    case GameState::GameEnd: {
+
+    }
+    break;
+    }
 
     m_renderer.display();
 }
@@ -64,7 +168,7 @@ void Game::initWindow()
 void Game::initViews()
 {
     m_views.addView(m_mainView);
-    m_views.addView(m_hudView);
+    m_views.addView(m_menuView);
 
     m_views.setInitialScreenSize(m_screenSize);
 }
@@ -72,11 +176,45 @@ void Game::initViews()
 void Game::initActions()
 {
     m_closeWindowAction.addCloseControl();
-    m_closeWindowAction.addKeycodeKeyControl(gf::Keycode::Escape);
     m_actions.addAction(m_closeWindowAction);
 
     m_fullscreenAction.addKeycodeKeyControl(gf::Keycode::F);
     m_actions.addAction(m_fullscreenAction);
+
+    m_leftClickAction.addMouseButtonControl(gf::MouseButton::Left);
+    m_actions.addAction(m_leftClickAction);
+}
+
+void Game::initWidgets()
+{
+    m_title.setAnchor(gf::Anchor::BottomCenter);
+    m_title.setColor(gf::Color::Black);
+    m_title.setPosition(gf::Vector2f{0.0f, -100.0f});
+
+    auto buttonInit = [this] (gf::TextButtonWidget& button, gf::Anchor anchor, const gf::Vector2f& posToCenter) {
+        button.setAnchor(anchor);
+        button.setPosition(posToCenter);
+
+        button.setDefaultBackgroundColor(gf::Color::Black);
+        button.setDefaultTextColor(gf::Color::White);
+
+        button.setPadding(10.0f);
+        button.setRadius(5.0f);
+
+        button.setState(gf::WidgetState::Default);
+
+        m_menuWidgets.addWidget(button);
+    };
+
+    buttonInit(m_playButton, gf::Anchor::BottomCenter, {0.0f, -20.0f});
+    m_playButton.setCallback([this] {
+        m_gameState = GameState::GameStart;
+    });
+
+    buttonInit(m_quitButton, gf::Anchor::TopCenter, {0.0f, 20.0f});
+    m_quitButton.setCallback([this] {
+        m_window.close();
+    });
 }
 
 void Game::initEntities()
